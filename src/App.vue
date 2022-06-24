3230d8c7f4050a6518a66b2f451590b96cebee63
cjnbennett <conor.jn.bennett@gmail.com>
2022-06-25 05:24:49 +0800

    Basic HTML and CSS implemented

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo App</h1>
+    <hr>
+    <div class="task-container">
+      <div class="task" v-for="n in 3" :key="n.value">
+        <h3 class="task-heading">Task {{n}}</h3>
+        <hr>
+        <strong>Description:</strong><br/>
+        This is a fun task! Task number {{n}}.
+        <div class="control-buttons">
+          <button class="completed-button">Mark Completed</button>
+          <button class="delete-button">Delete Task</button>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="task">
+        <h3 class="task-heading" contenteditable="true">New Task</h3>
+        <hr>
+        <strong>Description:</strong><br/>
+        <div contenteditable="true">
+        Enter description for new task.
+        </div>
+        <div class="control-buttons">
+          <button class="add-task-button">Add Task</button>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
@@ -24,5 +43,44 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  background-color: #111111;
+}
+
+.task-container {
+  height: 250px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.task {
+  height: 150px;
+  width: 200px;
+  margin: 10px;
+  background-color: lightgoldenrodyellow;
+  text-align: left;
+  font-size: 12px;
+  padding: 5px;
+  position: relative;
+}
+
+.task-heading {
+  color: #000000;
+  margin-top: 10px;
+  margin-bottom: -5px;
+  text-align: center;
+  font-size: 20px;
+}
+
+.control-buttons {
+  display: flex;
+  justify-content: space-evenly;
+  position: absolute;
+  bottom: 5px;
+  left: 0px;
+  right: 0px;
 }
 </style>
